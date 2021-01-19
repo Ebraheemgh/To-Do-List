674eb5a90456e370037468ee1caa41a0d503ee68
hadi moallem <hadimoallem1999@gmail.com>
2021-01-19 10:55:23 +0200

    first push

---
+++ test-helper.js
@@ -0,0 +1,29 @@
+function test(name, testFunction) {
+
+    function equal(x, y, message = `Expected ${x} to equal ${y}`) {
+      if (x === y) {
+        console.info("Pass: " + message);
+      } else {
+        console.error("Fail: " + message);
+      }
+    }
+  
+    function notEqual(x, y, message = `Expected ${x} not to equal ${y}`) {
+      if (x !== y) {
+        console.info("Pass: " + message);
+      } else {
+        console.error("Fail: " + message);
+      }
+    }
+    //assertions
+    const t = {
+      equal,
+      notEqual,
+    };
+  
+    console.group(name);
+    testFunction(t); 
+    console.groupEnd(name);
+  }
+  
+  
