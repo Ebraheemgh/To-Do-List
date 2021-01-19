0bdfc78e1d9c600c0bb51bcefc4bb61b794922b5
hadi moallem <hadimoallem1999@gmail.com>
2021-01-19 14:51:44 +0200

    hadiu

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var close = document.getElementsByClassName("close");
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
+var close = document.querySelectorAll("li .close:first-child");
+console.log(close);
+var list = document.querySelectorAll("li .close:nth-child(2)");
+var list1 = document.querySelectorAll("li");
 
 function newElement() {
     var li = document.createElement("li");
@@ -21,10 +16,31 @@ function newElement() {
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    var span2 = document.createElement("SPAN");
+    var txt2 = document.createTextNode("✓");
 
+    span.className = "close";
+    span2.className = "close";
+
+    span.appendChild(txt);
+    span2.appendChild(txt2);
+    li.appendChild(span);
+    li.appendChild(span2);
+    
+    close = document.querySelectorAll(".close:first-child");
+    list = document.querySelectorAll("li .close:nth-child(2)");
+
+     list1 = document.querySelectorAll("li");
+    for(let i=0 ; i<list.length; i++){
+        list[i].addEventListener('click', function(e) {
+               list1[i].classList.toggle('checked');
+           
+            
+            console.log(list[i]);
+        });
+        }
+
+    console.log(close);
     for (i = 0; i < close.length; i++) {
         console.log(close);
         close[i].onclick = function() {
